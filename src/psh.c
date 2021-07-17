98dc71dc2006ec91c8c3dc4469a89f234ba7c60a
ljte <ljte823@gmail.com>
2021-07-17 20:42:19 +0300

    add cd and exit builtins

--- src/psh.c
+++ src/psh.c
@@ -3,15 +3,44 @@
 #include "string.h"
 #include "unistd.h"
 #include "sys/wait.h"
+#include "limits.h"
 
 #define INC(var) (var)++
 #define trimLF(s) (s)[strcspn((s), "\n")] = '\0'
 #define SIZE_OF_STR sizeof(char *)
 
-void exitIfNull(const void *ptr, const char *errMsg) {
-    if (ptr == NULL) {
+char PREV_DIR[PATH_MAX];
+void __chdir__(const char *const dir) {
+    char temp[PATH_MAX];
+    if (getcwd(temp, sizeof(temp)) == NULL) {
+        perror("cd");
     }
+    if (chdir(dir) != 0) {
+        perror("cd");
+    }
+    strncpy(PREV_DIR, temp, PATH_MAX);
 }
+
+#define ROOT "/"
+int cd(char **args) {
+    if (args[1] == NULL) {
+        __chdir__(ROOT);
+    } else if (strcmp(args[1], "-") == 0) {
+        __chdir__(PREV_DIR);
+    } else {
+        __chdir__(args[1]);
+    }
+    return 1;
+}
+
+int _exit_(char **args) {
+    return 0;
+}
+
+const char *const builtins[] = {"cd", "exit"};
+int (*builtinHandler[]) (char **) = {&cd, &_exit_};
+
+#define NUMBER_OF_BUILTINS sizeof(builtins) / sizeof(char *)
 
 char *readLine(void) {
     size_t bufferSize = 0;
@@ -51,7 +80,16 @@ char **split(char *line, const char *delims) {
 
 }
 
-void execute(char **const args) {
+int execBuiltin(char **args) {
+    for (int i = 0; i < NUMBER_OF_BUILTINS; INC(i)) {
+        if (strcmp(args[0], builtins[i]) == 0) {
+            return builtinHandler[i](args);
+        }
+    }
+    return -1;
+}
+
+int execCmd(char **args) {
     int status = 0;
     const pid_t pid = fork();
 
@@ -67,6 +105,15 @@ void execute(char **const args) {
             waitpid(pid, &status, WUNTRACED);
         } while (!WIFEXITED(status) && !WIFSIGNALED(status));
     }
+    return 1;
+}
+
+int execute(char **args) {
+    int code = execBuiltin(args);
+    if (code != -1) {
+        return code;
+    }
+    return execCmd(args);
 }
 
 #define SPLIT_DELIMS " \t\r\n\a"
@@ -83,7 +130,9 @@ void mainLoop(void) {
             continue;
         }
         args = split(line, SPLIT_DELIMS);
-        execute(args);
+        if (execute(args) == 0) {
+            exit(EXIT_SUCCESS);
+        }
 
         free(line);
         free(args);
